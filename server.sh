c89316fed1155149a46501a5f173cb13bf37d066
luisherreratw <lherrera@thoughtworks.com>
2018-10-22 13:47:18 -0500

    updates server script for install plugin github

--- server.sh
+++ server.sh
@@ -20,6 +20,7 @@ else
 fi
 
 docker run --name server-gocd -d -p 8153:8153 -p 8154:8154 -v $(pwd)/godata:/godata -v $(pwd)/go:/home/go \
+-e GOCD_PLUGIN_INSTALL_github.pr=https://github.com/ashwanthkumar/gocd-build-github-pull-requests/releases/download/1.4.0-RC/github-pr-poller-1.4.0-RC.jar \
 --restart always gocd/gocd-server:v18.10.0
 
 echo 'GoCD server running OK :)'
